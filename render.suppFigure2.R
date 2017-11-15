170df8a3f9cc68d4f7f4296091a93c3a9858ebdf
Jianhong Ou <jianhongou@cwmwls055059.ad.umassmed.edu>
2017-11-15 16:18:44 -0500

    add render suppFigure2.R

---
+++ render.suppFigure2.R
@@ -0,0 +1,11 @@
+library(rmarkdown)
+render('suppFigure2.Rmd')
+x <- readLines('suppFigure2.tex')
+pos <- grep('begin\\{figure\\}', x)
+x[pos] <- gsub('begin\\{figure\\}', 'begin{figure}[!h]', x[pos])
+writeLines(x, 'suppFigure2.tex')
+tools::texi2pdf('suppFigure2.tex', clean = TRUE)
+
+file.remove('suppFigure2.tex')
+unlink("figure", recursive = TRUE)
+unlink("suppFigure2_files", recursive = TRUE)
