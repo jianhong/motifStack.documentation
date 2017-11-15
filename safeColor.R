2bc4fc11aa420c46fefe9fbe68b2debc3e2811cb
Jianhong Ou <jianhongou@Jianhongs-MacBook-Pro.local>
2017-11-14 19:03:46 -0500

    use magenta and turquoise

--- safeColor.R
+++ safeColor.R
@@ -1,7 +1,10 @@
 safeColor <- function(motifs){
-  DNAcolors <- c(A="#009E73", C="#0072B2", G="#E69F00", 'T'="#D55E00")
-  RNAcolors <- c(A="#009E73", C="#0072B2", G="#E69F00", 'U'="#D55E00")
-  map <- c("#00811B"="#009E73", "#2000C7"="#0072B2", "#FFB32C"="#E69F00", "#D00001"="#D55E00")
+  #DNAcolors <- c(A="#009E73", C="#0072B2", G="#E69F00", 'T'="#D55E00")
+  #RNAcolors <- c(A="#009E73", C="#0072B2", G="#E69F00", 'U'="#D55E00")
+  #map <- c("#00811B"="#009E73", "#2000C7"="#0072B2", "#FFB32C"="#E69F00", "#D00001"="#D55E00")
+  DNAcolors <- c(A="#40E0D0", C="#2000C7", G="#FFB32C", 'T'="#FF00FF")
+  RNAcolors <- c(A="#40E0D0", C="#2000C7", G="#FFB32C", 'U'="#FF00FF")
+  map <- c("#00811B"="#40E0D0", "#2000C7"="#2000C7", "#FFB32C"="#FFB32C", "#D00001"="#FF00FF")
   if(is(motifs, "list")){
     stopifnot(all(sapply(motifs, function(.ele) inherits(.ele, c("pcm", "pfm", "psam")))))
     lapply(motifs, function(.ele){
@@ -36,3 +39,12 @@ safeColor <- function(motifs){
     motifs
   }
 }
+
+
+# colorSet <- c("Dm"="#009E73", "b1h"="#009E73", "sw"="#008080", "bml"="darkgreen", 
+#               "Mm"="#D55E00", "MmDREAM"="#56B4E9", "Ms"="#E69F00", 
+#               "Hs"="#CC79A7")
+
+colorSet <- c("Dm"="#40E0D0", "b1h"="#40E0D0", "sw"="#008080", "bml"="darkgreen", 
+              "Mm"="brown", "MmDREAM"="blue", "Ms"="#F69156", 
+              "Hs"="#D900D9")
